ced1e325fd6e87066fad3dece16798bd52d91c94
Solomon Cammack <solomon@solomon.eu.com>
2021-06-04 10:51:50 +0100

    base datetime files

---
+++ datetime.cpp
@@ -0,0 +1,12 @@
+#include "mbed.h"
+#include "datetime.h"
+#include <iostream>
+
+int main() {
+    cout << "hi";
+    while (true) {
+        // wait for button to start 
+    }
+
+    return 0;
+}
---
+++ datetime.h
@@ -0,0 +1,5 @@
+#ifndef MBED_SLMN_DATETIME
+#define MBED_SLMN_DATETIME
+#include "mbed.h"
+
+#endif
